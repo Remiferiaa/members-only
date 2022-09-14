1a3337921b1786fbdca7dba5815fd03178b41f77
Remiferiaa <slong819@hotmail.com>
2022-09-14 16:42:26 +0800

    user model done

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    username: {type: String, required: true},
+    isMember: {type: Boolean, default: false},
+    isAdmin: {type: Boolean, default: false},
+})
+
+UserSchema
+    .virtual('url')
+    .get(() => {
+        return '/user/' + this._id
+    })
+
+module.exports = mongoose.model('User', UserSchema)

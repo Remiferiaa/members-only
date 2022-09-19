1bd4a9e1723d305f199d7c8a8b7e6bd071333530
Remiferiaa <slong819@hotmail.com>
2022-09-19 17:17:17 +0800

    basic user creation done

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const { body, validationResult } = require('express-validator')
+
+exports.user_create_get = (req, res, next) => {
+    res.render('form', { title: 'New User' })
+}
+
+exports.user_create_post = [
+    body('username').trim().isLength({ min: 4 }).withMessage("Username can't be shorter than 4 characters"),
+    body('password').trim().isLength({ min: 4 }).withMessage("Password can't be shorter than 4 characters"),
+
+    (req, res, next) => {
+        const errors = validationResult(req)
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        })
+        if (!errors.isEmpty()) {
+            res.render('form', { title: 'New User' , user, error: errors.array() })
+            return
+        } else {
+            bcrypt.hash(user.password, 10, (err, hashedPassword) => {
+                if (err) {
+                    return next(err)
+                } else {
+                    user.password = hashedPassword
+                    user.save((err) => {
+                        if (err) {
+                            return next(err)
+                        }
+                        res.redirect('/')
+                    })
+                }
+            })
+        }
+    }
+]
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const user_controller = require('../controllers/userController')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+router.get('/create', user_controller.user_create_get)
+
+router.post('/create', user_controller.user_create_post)
 
 module.exports = router;
